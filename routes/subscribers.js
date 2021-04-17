1607bdf21e86cff2eb88ebf1b1a275daee7ffedc
Vishnu Gp <vishnu.aassk@gmail.com>
2021-04-17 22:45:46 +0530

    Added middleware and corrected GET API

--- routes/subscribers.js
+++ routes/subscribers.js
@@ -13,8 +13,8 @@ router.get('/', async (req, res) => {
 })
 
 // Get one subscriber
-router.get('/:id', (req, res) => {
-
+router.get('/:id', getSubscriber, (req, res) => {
+    res.json(res.subscriber)
 })
 
 // Create one subscriber
@@ -40,5 +40,19 @@ router.patch('/:id', (req, res) => {
 router.delete('/:id', (req, res) => {
 
 })
+
+async function getSubscriber(req, res, next) {
+    try {
+        subscriber = await Subscriber.findById(req.params.id)
+        if (subscriber == null) {
+            return res.status(404).json({ message: 'Cant find subscriber' })
+        }
+    } catch (err) {
+        return res.status(500).json({ message: err.message })
+    }
+
+    res.subscriber = subscriber
+    next()
+}
 
 module.exports = router
